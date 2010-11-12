00d90899df3fed52396f968adec4b2715871247e
Giles Bowkett <gilesb@gmail.com>
2010-11-12 13:30:18 -0800

    does "hello world" via jade now

--- twitter.lisp
+++ twitter.lisp
@@ -1,7 +1,13 @@
-(defvar http (require 'http))
+(defvar http (require 'http)
+        sys (require 'sys)
+        node-static (require "./vendor/node-static/lib/node-static")
+        restler (require "./vendor/restler/lib/restler")
+        jade (require "./vendor/jade"))
 
 (chain (http.create-server (lambda (request response)
+                             (defun simple-output (error html)
+                               (response.end html))
                              (response.write-head 200 (hash "Content-type" "text/plain"))
-                             (response.end "hello world")))
+                             (jade.render-file "index.jade", (hash), simple-output)))
        (listen 8124 "127.0.0.1"))
 
