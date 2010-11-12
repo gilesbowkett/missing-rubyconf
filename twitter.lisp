e9577bc6e7645494a5ba7d48113bad88abf6d489
Giles Bowkett <gilesb@gmail.com>
2010-11-12 12:09:44 -0800

    better (more idiomatic)

--- twitter.lisp
+++ twitter.lisp
@@ -1,12 +1,7 @@
-(defvar sys (require 'sys)
-        utils (require 'utils)
-        http (require 'http)
-        node-static (require "./node-static/lib/node-static")
-        rest (require "./restler/lib/restler")
-        jade (require "./jade"))
+(defvar http (require 'http))
 
-(defvar server (http.create-server (lambda (request response)
-                                     (response.write-head 200 (hash "Content-type" "text/plain"))
-                                     (response.end "hello world"))))
-(server.listen 8124 "127.0.0.1")
+(chain (http.create-server (lambda (request response)
+                             (response.write-head 200 (hash "Content-type" "text/plain"))
+                             (response.end "hello world")))
+       (listen 8124 "127.0.0.1"))
 
